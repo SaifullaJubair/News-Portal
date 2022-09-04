cd6636e41e25f75c2830517c57fc11975f86ca24
SaifullaJubair <saifullajubair6@gmail.com>
2022-09-04 19:00:47 +0600

    loadALL news

--- JS/news.js
+++ JS/news.js
@@ -1,25 +1,9 @@
-// const loadCategorys = () => {
-//       fetch('https://openapi.programming-hero.com/api/news/categories')
-//             .then(res => res.json())
-//             .then(data => displayCategorys(data.data.news_category))
-// }
-
 const loadAllNewsCategories = async() => {
       const url = `https://openapi.programming-hero.com/api/news/categories`;
       const res = await fetch(url);
       const data = await res.json();
-      displayAllNewsCategories(data.data)
+     console.log(data.data.news_category)
+     // displayNewsCategories(data.data.news_category);
 }
 
-const displayAllNewsCategories = allNews => {
-      console.log(allNews)
-}
-
-// const newsCategory = document.getElementById('catagories-container')
-// const displayCategorys = categorys => {
-//       categorys.forEach(category => {
-//             console.log(category.category_name)
-//       });
-// }
-
-loadAllNewsCategories();
+loadAllNewsCategories()

86ea1caecf523c92b58fc68e25a33c3e7891a89c
SaifullaJubair <saifullajubair6@gmail.com>
2022-09-04 19:05:46 +0600

    add dynamic loadnewes

--- JS/news.js
+++ JS/news.js
@@ -18,4 +18,14 @@ const setAllNews = (data) => {
       }
 }
 
+const loadNewses = (id) => {
+      const url = `https://openapi.programming-hero.com/api/news/category/01`;
+      fetch(url)
+      .then(res => res.json())
+      .then(data => displayNews(data.data)) 
+      
+      
+}
+
+loadNewses()
 loadAllNewsCategories()

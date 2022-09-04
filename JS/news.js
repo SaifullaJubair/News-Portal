d5ffbe87fdf517aff515c9a7f058e42c1c541273
SaifullaJubair <saifullajubair6@gmail.com>
2022-09-04 19:03:39 +0600

    add dynamic news category

--- JS/news.js
+++ JS/news.js
@@ -2,8 +2,20 @@ const loadAllNewsCategories = async() => {
       const url = `https://openapi.programming-hero.com/api/news/categories`;
       const res = await fetch(url);
       const data = await res.json();
-     console.log(data.data.news_category)
+     setAllNews(data.data.news_category)
      // displayNewsCategories(data.data.news_category);
+}
+
+const setAllNews = (data) => {
+      // console.log(data);
+      const newsMenu = document.getElementById('all-menu')
+      for (const news of data) {
+            // console.log(news.category_name); 
+            const li = document.createElement('li');
+            li.innerHTML = `<a onclick = "loadNewses('id')" class="nav-link" href="#">${news.category_name}</a>`;
+            newsMenu.appendChild(li);
+            
+      }
 }
 
 loadAllNewsCategories()
